9b0e9c5e3533136c27dbaf9571450ba9169d58f9
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-05-07 17:22:47 -0500

    p-adic valuations

--- number_theory/elementary_NT/elementary_algorithms.go
+++ number_theory/elementary_NT/elementary_algorithms.go
@@ -1,6 +1,9 @@
 package elementaryNT
 
 
+import "fmt"
+
+
 func abs(a int) int {
 	if a < 0 {
 		return -a
@@ -36,4 +39,32 @@ func lcm(a, b int) int {
 }
 
 
+func pAdicVal(n int, p int) int, error {
+	if !isPrime(p) { // implement this
+		return 0, fmt.Errorf("%d is not prime", p)
+	}
+	prod = n
+	v_p = 0
+	while prod % p == 0 {
+		prod = prod / p
+		v_p += 1
+	}
+	return v_p, nil
+}
 
+
+func pAdicValFactorial(n int, p int) {
+	// returns the p-adic valuation of n!
+	if !isPrime(p) {
+		return 0, fmt.Errorf("%d is not prime", p)
+	}
+	if n < 0 {
+		return 0, fmt.Errorf("%d is negative, we not tryna use gamma to compute n!", n)
+	}
+  ans := 0
+  for n > 0 {
+       n /= p
+      ans += n
+  }
+  return ans
+}
