ef26c215a835707701202c8cff4db983a54fe9ff
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-05-08 15:17:54 -0500

    subgroup generation

--- algebraic_structures/groups/abstract_methods.go
+++ algebraic_structures/groups/abstract_methods.go
@@ -21,32 +21,40 @@ func Pow[T any](G Group[T], g T, n int) T {
 	}
 }
 
-func Commutator[T any](G Group[T], a T, b T) T {
+func (G Group[T]) Commutator[T any](a T, b T) T {
 	ab := G.Op(a, b)
 	ba := G.Op(b, a)
 	ba_inverse = G.Inverse(ba)
 	return G.Op(ab, ba_inverse)
 }
 
-func Commutes[T any](G Group[T], a T, b T) bool {
-	return G.Equals(Commutator(G, a, b), G.Identity())
+func (G Group[T]) Commutes[T any](a T, b T) bool {
+	return G.Equals(G.Commutator(a, b), G.Identity())
 }
 
-func (G AbstractGroup[T]) SubgroupGeneratedBy[T any](x T, max_iter int) Group[T] {
-	
-	if G.order != 0 {
-		accum = 
-		o := 1
-		while 
+func (G Group[T]) SubgroupGeneratedBy[T any](x T, max_iter int) []T, error {
+	if G.Order() < 0 {
+		return []T{}, fmt.Errorf("Error: Group order is negative.")
 	}
 	if max_iter <= 0 {
-
+		return []T{}, fmt.Errorf("Max iterations is not positive")
 	}
+	iterations = 0
+	elements := []T{G.Identity()}
+	current = x
+	for iterations <= max_iter {
+		if current == G.Identity {
+			break
+		}
+		elements = append(elements, current)
+		current = G.Op(current, x)
+	}
+	return elements, nil
 }
 
 
-func (G IsFinite() bool {
-
+func (G Group[T]) IsFinite[T any]() bool {
+	return G.Order() > 0
 }
 // Cayley tables?
 //
