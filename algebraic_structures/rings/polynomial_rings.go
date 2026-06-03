78d1314f492a2d5bf0c8fc0a892c4626b8a472b2
Ramzy Girgis <ramzy77girgis@gmail.com>
2026-06-03 10:17:24 -0400

    refactored polynomial sum, much nicer now

--- algebraic_structures/rings/polynomial_rings.go
+++ algebraic_structures/rings/polynomial_rings.go
@@ -1,13 +1,41 @@
 package rings
 
 type Polynomial[T any] struct {
+	ring Ring[T]
 	coefficients []T
-	degree int
+	// degree int // maybe remove
+}
+
+func (R Ring[Polynomial[T]]) Zero[T any]() T {
+	ring_zero := R.ring.Zero()
+	return Polynomial[T]{ring: R, coefficients: []T{ring_zero}} 
+}
+
+func (R Ring[Polynomial[T]]) Add[T any] (f, g Polynomial[T]) Polynomial[T] {
+	deg_f := len(f.coefficients) - 1
+	deg_g := len(g,coefficients) - 1
+	max_len := max(deg_f + 1, deg_g + 1)
+	sum_coeffs := make([]T, max_len)
+	for i := 0; i < max_len; i++ {
+		sum_coeffs[i] = f.coefficients[i] + g.coefficients[i] 
+	}
+	leading_index = max_len - 1
+	while sum_coeffs[leading_index] == 0 {
+		if leading_index == 0 {
+			return R.Zero[T]()
+		}
+		leading_index -= 1
+	}
+	coeffs = sum_coeffs[:leading_index + 1]
+	return Polynomial[T]{ring: R, coefficients: coeffs}
+	}
 }
 
 
 
-func (R Ring) PolynomialRing() Ring[list[T]] {
+
+
+func (R Ring) PolynomialRing() Ring[[]list] {
 
 }
 
